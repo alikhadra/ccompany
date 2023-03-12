acde36b66653b0a077bd869ab706d58cf736f74a
Ali Khadra <alikhadra15@gmail.com>
2023-03-12 12:17:15 +0200

    ccompany project

--- src/index.js
+++ src/index.js
@@ -1,21 +1,16 @@
-import './scss/style.scss';
-import 'bootstrap/dist/css/bootstrap.rtl.min.css';
-import './css/style.css';
-// import 'jquery/dist/jquery.min';   //this import dosn't work 
-import $ from "jquery";              // this is alternative import
-import 'popper.js/dist/popper.min';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import '@fortawesome/fontawesome-free/js/all.min';
+import "./scss/style.scss";
+import "bootstrap/dist/css/bootstrap.rtl.min.css";
+import "./css/style.css";
+// import 'jquery/dist/jquery.min';   //this import dosn't work
+import $ from "jquery"; // this is alternative import
+import "popper.js/dist/popper.min";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "@fortawesome/fontawesome-free/js/all.min";
 
-
-$(function () {  
-
+$(function () {
     $('[data-toggle="tooltip"]').tooltip();
 
-    $('.add-to-cart-btn').on("click", function () {
-        alert('أضيف المُنتج إلى السلة');   
-
+    $(".add-to-cart-btn").on("click", function () {
+        alert("أضيف المُنتج إلى السلة");
     });
 });
-
-
